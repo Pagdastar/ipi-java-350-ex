6fac1ecc3f9cfb8d3b3bfbdeff0e154632f1660b
Paul Goncalves <paul.goncalves@cgi.com>
2019-03-14 15:34:59 +0100

    Premiers tests

--- src/test/java/com/ipiecoles/java/java350/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/EmployeTest.java
@@ -20,4 +20,45 @@ public class EmployeTest {
         //Then
         Assertions.assertThat(nbAnnee).isEqualTo(0);
     }
+
+    @Test
+    public void getNombreAnneeAncienneteNull(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(null);
+
+        //When
+        Integer nbAnnee = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+    @Test
+    public void getNombreAnneeNMoinsDeux(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(2));
+
+        //When
+        Integer nbAnnee = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(2);
+    }
+
+    @Test
+    public void getNombreAnneeNPlusDeux(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().plusYears(2));
+
+        //When
+        Integer nbAnnee = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+
 }
